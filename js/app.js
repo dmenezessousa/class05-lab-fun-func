82f56d2952d64ba1053f3b07d0de2e48642d465a
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-08-22 09:57:56 -0400

    multiplyArray Func

--- js/app.js
+++ js/app.js
@@ -105,11 +105,11 @@ function sumArray(sumArr) {
   ];
 }
 
-console.log(sumArray(testArray));
+// console.log(sumArray(testArray));
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -126,10 +126,23 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  let result = multiply(multArr[0], multArr[1], multArr[2]);
+  return [
+    result,
+    "The numbers " +
+    multArr[0] +
+      "," +
+      multArr[1] +
+      "," +
+      multArr[2] +
+      " have a product of " +
+      result +
+      ".",
+  ];
 }
-
+console.log(multiplyArray(testArray));
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
